cac925f2553ec77e69781092b0a7c591a397537d
Thupha41 <phatngo.040103@gmail.com>
2024-12-10 22:22:27 +0700

    Set up folder for source project

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-const fullName: string = 'Ngo Thuan Phat'
-console.log(fullName)
+import express from 'express'
+import dotenv from 'dotenv'
+import initApiRoute from './routes/api'
+
+//Load .env from dotenv config
+dotenv.config()
+
+const app = express()
+//Parse json body to object
+app.use(express.json())
+const PORT = process.env.PORT
+
+type Number = {
+  a: number
+  b: number
+}
+initApiRoute(app)
+const sum = (obj: Number) => {
+  return obj.a + obj.b
+}
+const value = sum({ a: 1, b: 2 })
+
+app.get('/', (req, res) => {
+  res.send(`Hello World. Your value is ${value}`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
